658c8cf8eb2c1da50dbd896d52bda362b01fbf0e
lillie <onlinechronically@protonmail.com>
2024-12-26 01:55:18 -0500

    add: different types of actions for the heap queue

--- heap_queue.lua
+++ heap_queue.lua
@@ -4,16 +4,18 @@ local ActionQueue = {
     size = 0
 }
 
-function Enqueue(entry)
+function Enqueue(type, entry)
     if ActionQueue.size == 0 then
         ActionQueue.first = {
             value = entry,
+            type = type,
             next = nil
         }
         ActionQueue.last = ActionQueue.first
     else
         ActionQueue.last.next = {
             value = entry,
+            type = type,
             next = nil
         }
         ActionQueue.last = ActionQueue.last.next
@@ -25,10 +27,11 @@ local function dequeue()
     if ActionQueue.size == 0 then
         return nil
     else
-        local first = ActionQueue.first.value
+        local first = ActionQueue.first
+        if not first then return nil end
         if not ActionQueue.first.next then ActionQueue.last = nil end
         ActionQueue.first = ActionQueue.first.next
         ActionQueue.size = ActionQueue.size - 1
-        return first
+        return { value = first.value, type = first.type }
     end
 end
